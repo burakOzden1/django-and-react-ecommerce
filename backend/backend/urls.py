288c0c374157e00b7eaf9710f0f3c143abfb6b38
Zeki Burak Özden <zburakozden@gmail.com>
2024-03-22 16:35:29 +0300

    api dokumantasyonu kullanimi

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,9 +19,29 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="E-commerce Backend APIs",
+        default_version="v1",
+        description="Bu dokümantasyon backend API'sidir",
+        terms_of_service="http://mywbsite.com/policies/",
+        contact=openapi.Contact(email="admin@admin.com"),
+        license=openapi.License(name="BSD Licence"),
+    ),
+    public = True,
+    permission_classes = (permissions.AllowAny, )
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
+
+    # Documentation
+    path("", schema_view.with_ui('swagger', cache_timeout=0), name="schema-swagger-ui")
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
