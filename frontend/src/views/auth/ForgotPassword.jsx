e7e31e2e35dbe9572fadfcc91812d8a300b962e9
Zeki Burak Özden <zburakozden@gmail.com>
2024-04-08 00:52:16 +0300

    auth islemlerinin bootstrap yapilari eklendi. Calismayan fonksiyonlar var, onlara bakilacak.

--- frontend/src/views/auth/ForgotPassword.jsx
+++ frontend/src/views/auth/ForgotPassword.jsx
@@ -1,36 +1,81 @@
 import { useState } from 'react'
 import apiInstance from '../../utils/axios'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
 
 function ForgotPassword() {
     const [email, setEmail] = useState("")
+    const [isLoading, setIsLoading] = useState(false)
 
     const navigate = useNavigate()
 
     const handleSubmit = async () => {
+        setIsLoading(true)
         try {
             await apiInstance.get(`user/password-reset/${email}/`).then((res) => {
-                alert("Mail adresinize bir e-posta gönderildi.")
-                navigate("/create-new-password")
+                alert("An Email Has been Sent to you.")
+                setIsLoading(false)
             })
         } catch (error) {
-            alert("Email does not exists")
+            alert("Email Does Not Exists")
+            setIsLoading(false)
         }
     }
 
     return (
         <div>
-            <h1>Şifremi Unuttum</h1>
-            <input 
-                onChange={(e) => setEmail(e.target.value)} 
-                type='email' 
-                placeholder='Mail adresinizi giriniz...' 
-                name='' 
-                id='' 
-            />
-            <br />
-            <br />
-            <button onClick={handleSubmit}>Şifremi Yenile</button>
+            <section>
+                <main className="" style={{ marginBottom: 100, marginTop: 50 }}>
+                    <div className="container">
+                        {/* Section: Login form */}
+                        <section className="">
+                            <div className="row d-flex justify-content-center">
+                                <div className="col-xl-5 col-md-8">
+                                    <div className="card rounded-5">
+                                        <div className="card-body p-4">
+                                            <h3 className="text-center">Login</h3>
+                                            <br />
+
+                                            <div className="tab-content">
+                                                <div
+                                                    className="tab-pane fade show active" id="pills-login" role="tabpanel" aria-labelledby="tab-login">
+                                                    <div>
+                                                        {/* Email input */}
+                                                        <div className="form-outline mb-4">
+                                                            <label className="form-label" htmlFor="Full Name">
+                                                                Email 
+                                                            </label>
+                                                            <input type="text" id="username" name="email" value={email} className="form-control" onChange={(e) => setEmail(e.target.value)}/>
+                                                        </div>
+
+                                                        {isLoading === true
+                                                            ? <button disabled type='button' className='btn btn-primary btn-rounded w-100 mb-4'>
+                                                                Processing...
+                                                            </button>
+
+                                                            : <button onClick={handleSubmit} type='button' className='btn btn-primary btn-rounded w-100 mb-4'>
+                                                                Send Email <i className='fas fa-paper-plane'></i>
+                                                            </button>
+                                                        }
+
+                                                        <div className="text-center">
+                                                            <p className='mt-4'>
+                                                                Want to sign in? <Link to="/login">Login</Link>
+                                                            </p>
+                                                            <p className='mt-0'>
+                                                                <Link to="/forgot-password/" className='text-danger'>Forgot Password?</Link>
+                                                            </p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </main>
+            </section>
         </div>
     )
 }
--- frontend/src/views/auth/Login.jsx
+++ frontend/src/views/auth/Login.jsx
@@ -30,42 +30,77 @@ function Login() {
 
         const { error } = login(email, password)
         if (error) {
+            setIsLoading(false)
             alert(error)
         } else {
             navigate("/")
             resetForm()
+            setIsLoading(false)
         }
-        setIsLoading(false)
     }
 
   return (
-    <div>
-        <h2>Hoşgeldiniz</h2>
-        <p>Giriş Yap</p>
-        <form onSubmit={ handleLogin }>
-            <input 
-                type="text"
-                name="email"
-                id="email"
-                value={ email }
-                onChange = {(e) => setEmail(e.target.value)}
-            />
-            <br/>
-            <br/>
-            <input 
-                type="password"
-                name="password"
-                id="password"
-                value={ password }
-                onChange = {(e) => setPassword(e.target.value)}
-            />
-            <br/>
-            <br/>
-            <button type='submit'>Giriş Yap</button>
-            <hr />
-            <Link to={'/forgot-password'}>Parolamı Unuttum</Link>
-        </form>
-    </div>
+
+    <>
+        <section>
+            <main className="" style={{ marginBottom: 100, marginTop: 50 }}>
+                <div className="container">
+                    {/* Section: Login form */}
+                    <section className="">
+                        <div className="row d-flex justify-content-center">
+                            <div className="col-xl-5 col-md-8">
+                                <div className="card rounded-5">
+                                    <div className="card-body p-4">
+                                        <h3 className="text-center">Login</h3>
+                                        <br />
+
+                                        <div className="tab-content">
+                                            <div
+                                                className="tab-pane fade show active" id="pills-login" role="tabpanel" aria-labelledby="tab-login">
+                                                <form onSubmit={handleLogin}>
+                                                    {/* Email input */}
+                                                    <div className="form-outline mb-4">
+                                                        <label className="form-label" htmlFor="Full Name">
+                                                            Email Address
+                                                        </label>
+                                                        <input type="text" id="username" name="email" value={email} className="form-control" onChange={(e) => setEmail(e.target.value)}/>
+                                                    </div>
+
+                                                    <div className="form-outline mb-4">
+                                                        <label className="form-label" htmlFor="loginPassword">Password</label>
+                                                        <input type="password" id="password" name="password" value={password} className="form-control" onChange={(e) => setPassword(e.target.value)} />
+                                                    </div>
+
+                                                    {isLoading === true 
+                                                        ? <button className='btn btn-primary w-100' type="submit">
+                                                            <span className="mr-2">Processing</span>
+                                                            <i className="fas fa-spinner fa-spin" />
+                                                            </button>
+                                                        : <button className='btn btn-primary btn-rounded w-100 mb-4' type="submit">
+                                                            Sign In <i className="fas fa-sign-in" />
+                                                        </button>
+                                                    }
+
+                                                    <div className="text-center">
+                                                        <p className='mt-4'>
+                                                            Don't have an account? <Link to="/register">Register</Link>
+                                                        </p>
+                                                        <p className='mt-0'>
+                                                            <Link to="/forgot-password/" className='text-danger'>Forgot Password?</Link>
+                                                        </p>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </main>
+        </section>
+    </>
   )
 }
 
